dd21655b8d91d4ca6d2912cf57f59310cb9954bb
angelokapunda <angelokapunda@gmail.com>
2026-03-15 09:43:24 -0300

    :sparkles: Endpoint interceptor de token JWT adicionado

---
+++ product-api /src/main/java/br/com/microsservissos/product_api/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,30 @@
+package br.com.microsservissos.product_api.config.interceptor;
+
+import br.com.microsservissos.product_api.modules.jwt.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(isOptions(request)) {
+            return true;
+        }
+        var authorization = request.getHeader(AUTHORIZATION);
+        jwtService.validateAuthorizetionk(authorization);
+        return true;
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return HttpMethod.OPTIONS.name().equals(request.getMethod());
+    }
+}
