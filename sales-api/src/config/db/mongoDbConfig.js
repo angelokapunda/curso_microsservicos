743ca34221e865ba1fb12b2c8057761da6b0ac53
angelokapunda <angelokapunda@gmail.com>
2026-03-29 19:40:54 -0300

    :sparkles: Configuração do MongoDB

---
+++ sales-api/src/config/db/mongoDbConfig.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+import { MONGO_DB_URL } from "../secrets/secrets.js";
+
+export function connect() {
+    mongoose.connect(MONGO_DB_URL);
+    mongoose.connection.on("connected", function() {
+        console.info("The application connected to MongoDB successFully");
+    });
+    mongoose.connection.on("error", function() {
+        console.error("The application connected to MongoDB successFully");
+    });
+}
